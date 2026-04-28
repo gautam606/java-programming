fb6cc4c4df8f2892201b57ad90cac5c8a797c40c
gautam606 <gauravsk615@gmail.com>
2026-04-28 18:07:53 +0530

    first commit

---
+++ practice_code/src/add.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class add {
+    public static void main(String[] args)
+    throws Exception{
+        float a=233.43f;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        float b=sc.nextFloat();
+        System.out.println(a+b);
+
+    }
+}
---
+++ practice_code/src/asci.java
@@ -0,0 +1,8 @@
+public class asci {
+    public static void main(String[] args)
+    throws Exception{
+        char ch='a';
+        int charassi= (int) ch;
+        System.out.println(charassi);
+    }
+}
