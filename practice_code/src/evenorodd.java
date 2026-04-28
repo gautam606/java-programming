7a5c4fc4eb76cc6112cf5c6995e47053dbef9acd
gautam606 <gauravsk615@gmail.com>
2026-04-29 01:36:46 +0530

    added new programs

---
+++ practice_code/src/evenorodd.java
@@ -0,0 +1,13 @@
+public class evenorodd {
+    public static void main(String[] args) {
+        int num=18;
+        if(num%2==0)
+        {
+            System.out.println(num+" is even number");
+        }
+        else {
+
+            System.out.println(num+" is even number");
+        }
+    }
+}
