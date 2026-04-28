3ac69a868545d801d2dc7acc4ab148d9a32eef9b
gautam606 <gauravsk615@gmail.com>
2026-04-29 01:33:12 +0530

    added new programs

---
+++ practice_code/src/quotientremainder.java
@@ -0,0 +1,10 @@
+public class quotientremainder {
+    public static void main(String[] args) {
+        int a=10,b=2;
+        int quotient=a/b;
+        int remainder=a%b;
+        System.out.println(quotient);
+        System.out.println(remainder);
+
+    }
+}
---
+++ practice_code/src/swap.java
@@ -0,0 +1,14 @@
+public class swap {
+    public static void main(String[] args) {
+        float a=53442.45f;
+        float b=56334.34f;
+        float temp;
+        System.out.println("first number:"+a+"second number:"+b);
+
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("first number:"+a+"second number:"+b);
+
+    }
+}
