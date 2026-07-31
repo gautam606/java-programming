f3805e99d70cc2197e4e26f57825a73143211621
gautam606 <gauravsk615@gmail.com>
2026-07-31 13:06:41 +0530

    update commit

---
+++ strings/strings/src/p_15.java
@@ -0,0 +1,25 @@
+public class p_15 {
+    public static void main(String[] args) {
+        // Example inputs
+        String s1 = "Hello World";
+        String s2 = "   fly me   to   the moon  ";
+        String s3 = "luffy is still joyboy";
+
+        lengthOfLastWord(s1);
+        lengthOfLastWord(s2);
+        lengthOfLastWord(s3);
+    }
+
+    public static void lengthOfLastWord(String s) {
+
+        s = s.trim();
+
+
+        int lastSpace = s.lastIndexOf(' ');
+
+
+        int length = s.length() - lastSpace - 1;
+        
+        System.out.println(length);
+    }
+}
