526d90296ed23c065c6770244dcfc86dcbde59d7
gautam606 <gauravsk615@gmail.com>
2026-08-04 22:39:36 +0530

    update commit

---
+++ strings/strings/src/p_18.java
@@ -0,0 +1,13 @@
+public class p_18 {
+    public static void main(String[] args) {
+        String str="Hello World";
+        String[] s=str.split(" ");
+        String res=s[s.length-1];
+        System.out.println(res.length());
+    }
+}
+
+//Input:"Hello World"
+//
+//Output:5
+
